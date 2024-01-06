6c63484fd24e63ef991b6fb71c494f714ac7b634
Helen Gezahegn <gezahegn@ualberta.ca>
2024-01-05 23:56:14 -0700

    feat: added signup screen + toggle fxnality

--- netflix-clone/pages/auth.tsx
+++ netflix-clone/pages/auth.tsx
@@ -1,10 +1,17 @@
 import Input from "@/components/Input";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant == "login" ? "register" : "login"
+    );
+  }, []);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -14,14 +21,18 @@ const Auth = () => {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant == "login" ? "Sign in" : "Register"}
+            </h2>
             <div className="flex flex-col gap-4">
-              <Input
-                label="Username"
-                onChange={(e: any) => setUsername(e.target.value)}
-                id="username"
-                value={username}
-              />
+              {variant == "register" && (
+                <Input
+                  label="Username"
+                  onChange={(e: any) => setUsername(e.target.value)}
+                  id="username"
+                  value={username}
+                />
+              )}
               <Input
                 label="Email"
                 onChange={(e: any) => setEmail(e.target.value)}
@@ -38,12 +49,17 @@ const Auth = () => {
               />
             </div>
             <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-              Login
+              {variant == "login" ? "Login" : "Register"}
             </button>
             <p className="text-neutral-500 mt-12">
-              First time using Netflix?
-              <span className="text-white ml-1 hover:underline cursor-pointer">
-                Create an account.
+              {variant == "login"
+                ? "New to Netflix?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer font-extralight"
+              >
+                {variant == "login" ? "Sign up now." : "Sign in now."}
               </span>
             </p>
           </div>
